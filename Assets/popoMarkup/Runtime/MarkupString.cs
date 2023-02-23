cdf85391ed00fe27bf2c4c54d4ad00bd1899b9f6
KYapp <kukiyomenaisapp@gmail.com>
2023-02-23 16:52:32 +0900

    MarkupStringを定義

---
+++ Assets/popoMarkup/Runtime/MarkupString.cs
@@ -0,0 +1,10 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace JuhaKurisu.PopoTools.Markup
+{
+    public class MarkupString
+    {
+    }
+}
