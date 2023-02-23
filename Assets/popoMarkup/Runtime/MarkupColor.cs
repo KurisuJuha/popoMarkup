dfbf4b831963614b5d6c368fbcb43dca00a9e1d5
KYapp <kukiyomenaisapp@gmail.com>
2023-02-23 15:00:04 +0900

    MarkupColor

---
+++ Assets/popoMarkup/Runtime/MarkupColor.cs
@@ -0,0 +1,10 @@
+namespace JuhaKurisu.PopoTools.Markup
+{
+    public class MarkupColor
+    {
+        public readonly byte r;
+        public readonly byte g;
+        public readonly byte b;
+        public readonly byte a;
+    }
+}
