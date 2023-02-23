7276a329c5cb502c65004355cefe19c65eb115d6
KYapp <kukiyomenaisapp@gmail.com>
2023-02-23 16:58:37 +0900

    ColorStyle

---
+++ Assets/popoMarkup/Runtime/Styles/ColorStyle.cs
@@ -0,0 +1,12 @@
+namespace JuhaKurisu.PopoTools.Markup
+{
+    public class ColorStyle : Style
+    {
+        public readonly MarkupColor color;
+
+        public ColorStyle(MarkupColor color)
+        {
+            this.color = color;
+        }
+    }
+}
