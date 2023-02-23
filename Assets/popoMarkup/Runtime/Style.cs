1b8b70f4f999da04cefcafdfdc57465603d38ef4
KYapp <kukiyomenaisapp@gmail.com>
2023-02-23 16:58:03 +0900

    Styleにインデクサ定義

--- Assets/popoMarkup/Runtime/Style.cs
+++ Assets/popoMarkup/Runtime/Style.cs
@@ -2,6 +2,10 @@ namespace JuhaKurisu.PopoTools.Markup
 {
     public abstract class Style
     {
+        public MarkupString this[string t]
+        {
+            get => new MarkupString().AddText(t, this);
+        }
 
         public virtual string ToString(string text)
             => text;
